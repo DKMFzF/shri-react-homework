625c6b2ff486e5ea570ad778756f42b60198c81d
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-06-18 18:27:49 +0700

    init commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import styles from './App.module.css';
+
+export const App = () => {
+  return (
+    <div className={styles['page-wrapper']}>
+      Привет мир!
+    </div>
+  )
+}
+
+export default App
