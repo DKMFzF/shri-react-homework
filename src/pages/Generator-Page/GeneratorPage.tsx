954249a7b37160eb788412b49095752e2e56092e
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-06-20 15:54:34 +0700

    feat: added content for generation-page

--- src/pages/Generator-Page/GeneratorPage.tsx
+++ src/pages/Generator-Page/GeneratorPage.tsx
@@ -1,5 +1,11 @@
+import { Button } from '../../components/ui';
+import styles from './GeneratorPage.module.css';
+
 export const GeneratorPage = () => {
   return (
-      <>GeneratorPage</>
+      <div className={styles['generator-page']}>
+        Сгенерируйте готовый csv-файл нажатием одной кнопки
+        <Button type='send'>Начать Генерацию</Button>
+      </div>
   );
 }
